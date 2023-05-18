b38f728fcfac165e90618a7b23a94962a8c0b05a
Alexey Timofeev <lesha.timofeev2017@yandex.ru>
2023-05-18 11:37:06 +0300

    Initial commit

---
+++ src/test/java/com/example/profgps/ProfGpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfGpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
